26953aaf7fe5e246f22e608f022f20736c6487ff
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2025-03-12 22:42:48 +0100

    image lazy loading and h1 in every page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,12 @@
 import { Routes } from '@angular/router';
-import { MembershipComponent } from './components/membership/membership.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { ScheduleComponent } from './components/schedule/schedule.component';
 import { LegalNoticesComponent } from './components/legal-notices/legal-notices.component';
 import { LocalComponent } from './components/local/local.component';
-import { MemberPageComponent } from './components/member-page/member-page.component';
-import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { CalendrierComponent } from './components/calendrier/calendrier.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  {
-    path: 'achat-atelier',
-    component: MemberPageComponent,
-  },
   {
     path: 'acces-local',
     component: LocalComponent,
@@ -27,11 +19,6 @@ export const routes: Routes = [
     path: 'contact',
     component: ContactComponent,
   },
-  {
-    path: 'admin/login',
-    component: AdminLoginComponent,
-  },
-
   {
     path: 'mentions-legales',
     component: LegalNoticesComponent,
